453f5b1bf73c78d481b07e50c632266841be23b9
Rena Upadhyay <renaupadhyay2022@u.northwestern.edu>
2020-07-14 21:10:34 +0000

    attempt to fix errors on both brnaches

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -13,6 +13,12 @@
 // limitations under the License.
 
 package com.google.sps.servlets;
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -30,26 +36,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 
-/** Servlet that returns data of comments*/
+/** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-    private final Gson gson = new Gson();
 
-    public static class Comment { 
+    public static class Comment{ 
         private final String name; 
         private final String email;
         private final String comment;
         private final long timestamp; 
         private final long id;
+        private final double score;
   
-        public Comment(String name, String email, String comment, long timestamp, long id) {
+        public Comment(String name, String email, String comment, long timestamp, long id, double score) {
             this.name = name;
             this.email = email;
             this.comment = comment;
             this.timestamp =timestamp;
-            this.id= id;
+            this.id = id;
+            this.score = score;
         }
     }
+
+    
     
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -58,9 +67,10 @@ public class DataServlet extends HttpServlet {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
 
-    String userChoice = request.getParameter("num");
+    String numChoice = request.getParameter("num");
+    String languageChoice = request.getParameter("language");
     int maxComments;
-    maxComments = Integer.parseInt(userChoice);
+    maxComments = Integer.parseInt(numChoice);
     int i =0;
     for (Entity entity : results.asIterable()) {
         if (i < maxComments){
@@ -69,16 +79,23 @@ public class DataServlet extends HttpServlet {
             String email = (String) entity.getProperty("email");
             String comment = (String) entity.getProperty("comment");
             long timestamp = (long) entity.getProperty("timestamp");
+            double score = (double) entity.getProperty("score");
+
+            Translate translate = TranslateOptions.getDefaultInstance().getService();
+            Translation translation =
+                translate.translate(comment, Translate.TranslateOption.targetLanguage(languageChoice));
+            String translatedText = translation.getTranslatedText();
         
-            Comment userComment = new Comment(name, email, comment,timestamp,id);
-                    comments.add(0,userComment);        
+            Comment user_comment = new Comment(name, email, translatedText,timestamp,id, score);
+                comments.add(0,user_comment);        
         }
         i++;
     }
     
 
-    String json = gson.toJson(comments);
+    String json = convertToJsonUsingGson(comments);
     response.setContentType("application/json;");
+    response.setCharacterEncoding("UTF-8");
     response.getWriter().println(json);
   }
 
@@ -93,19 +110,46 @@ public class DataServlet extends HttpServlet {
     String comment = request.getParameter("comment");
     long timestamp = System.currentTimeMillis();
 
+    Document doc =
+        Document.newBuilder().setContent(comment).setType(Document.Type.PLAIN_TEXT).build();
+    LanguageServiceClient languageService = LanguageServiceClient.create();
+    Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
+    float score = sentiment.getScore();
+    languageService.close();
+
+    
+
     Entity commentEntity = new Entity("Comment");
     commentEntity.setProperty("name", name);
     commentEntity.setProperty("email", email);
     commentEntity.setProperty("comment", comment);
     commentEntity.setProperty("timestamp", timestamp);
+    commentEntity.setProperty("score",score);
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(commentEntity);
     response.sendRedirect("/contact.html");
   }
 
-//left this the same
-private String getUserNickname(String id) {
+  
+  private String convertToJson(ArrayList<String> messages) {
+    String json = "[";
+    json += "\"" + messages.get(0) + "\"";
+    for (int i=1; i<messages.size(); i++){
+        json += ", ";
+        json += "\"" + messages.get(i) + "\"";
+    }
+    json += "]";
+    return json;
+  }
+
+  private String convertToJsonUsingGson(ArrayList<Comment> comments) {
+    Gson gson = new Gson();
+    String json = gson.toJson(comments);
+    return json;
+  }
+
+   private String getUserNickname(String id) {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     Query query =
         new Query("UserInfo")
@@ -120,6 +164,11 @@ private String getUserNickname(String id) {
   }
 
 }
+
+
+
+
+
 
 
 
